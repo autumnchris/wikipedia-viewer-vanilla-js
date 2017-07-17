cd1c99c1d11dbeffdcae80499e8ae9de421a246b
Autumn <autumncbullard@gmail.com>
2017-07-17 05:58:23 -0400

    Changed the AJAX syntax to the .done() method

--- js/script.js
+++ js/script.js
@@ -3,31 +3,29 @@ function loadData() {
   $('#results').html('');
 
   $.ajax({
+    dataType: 'jsonp',
     url: 'https://en.wikipedia.org/w/api.php?callback=?',
     data: {
       action: 'query',
       list: 'search',
       srsearch: $('#search-field').val(),
       format: 'json'
-    },
-    dataType: 'jsonp',
-    success: function(wikiData) {
-      var searchResults = wikiData.query.search;
-
-      for (var i = 0; i < searchResults.length; i++) {
-        var articleTitle = searchResults[i].title;
-        var articleSnippet = searchResults[i].snippet;
-        var articleURL = 'https://en.wikipedia.org/wiki/' + articleTitle;
-        var entries = '<div class="well center-block"><h2>' + articleTitle + '</h2><p>' + articleSnippet + '...</p><a href="' + articleURL + '" target="_blank">Continue Reading...</a></div>';
-        $('#results').append(entries);
-      }
-    },
-    error: function() {
-      $('#results').html('<div class="alert alert-warning text-center"><span class="fa fa-warning fa-lg fa-fw"></span> Unable to load Wikipedia search results.</div>');
     }
+  }).done(function(wikiData) {
+    var searchResults = wikiData.query.search;
+
+    for (var i = 0; i < searchResults.length; i++) {
+      var articleTitle = searchResults[i].title;
+      var articleSnippet = searchResults[i].snippet;
+      var articleURL = 'https://en.wikipedia.org/wiki/' + articleTitle;
+      var entries = '<div class="well center-block"><h2>' + articleTitle + '</h2><p>' + articleSnippet + '...</p><a href="' + articleURL + '" target="_blank">Continue Reading...</a></div>';
+      $('#results').append(entries);
+    }
+  }).fail(function() {
+    $('#results').html('<div class="alert alert-warning text-center"><span class="fa fa-warning fa-lg fa-fw"></span> Unable to load Wikipedia search results.</div>');
   });
 
   return false;
-};
+}
 
 $('form').submit(loadData);
