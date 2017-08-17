d61690bb63cb81f383eed9c4aa6252293c92a5a9
Autumn <autumncbullard@gmail.com>
2017-08-17 13:49:55 -0400

    Add error handling in case the search input doesn't return any results

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 function loadData() {
 
+  var searchInput = $('#search-input').val();
   $('#results').empty();
 
   $.ajax({
@@ -8,18 +9,24 @@ function loadData() {
     data: {
       action: 'query',
       list: 'search',
-      srsearch: $('#search-field').val(),
+      srsearch: searchInput,
       format: 'json'
     }
   }).done(function(wikiData) {
     var searchResults = wikiData.query.search;
 
-    for (var i = 0; i < searchResults.length; i++) {
-      var articleTitle = searchResults[i].title;
-      var articleSnippet = searchResults[i].snippet;
-      var articleURL = 'https://en.wikipedia.org/wiki/' + articleTitle;
-      var entries = '<article><div class="well center-block"><h2>' + articleTitle + '</h2><p>' + articleSnippet + '...</p><a href="' + articleURL + '" target="_blank">Continue Reading...</a></div></article>';
-      $('#results').append(entries);
+    if (searchResults.length === 0) {
+      $('#results').html('<div class="alert alert-warning text-center"><span class="fa fa-warning fa-lg fa-fw"></span> Unable to find results for ' + '\"' + searchInput + '\"' + '. Consider revising your search.</div>');
+    }
+
+    else {
+      for (var i = 0; i < searchResults.length; i++) {
+        var articleTitle = searchResults[i].title;
+        var articleSnippet = searchResults[i].snippet;
+        var articleURL = 'https://en.wikipedia.org/wiki/' + articleTitle;
+        var entries = '<article><div class="well center-block"><h2>' + articleTitle + '</h2><p>' + articleSnippet + '...</p><a href="' + articleURL + '" target="_blank">Continue Reading...</a></div></article>';
+        $('#results').append(entries);
+      }
     }
   }).fail(function() {
     $('#results').html('<div class="alert alert-warning text-center"><span class="fa fa-warning fa-lg fa-fw"></span> Unable to load Wikipedia search results.</div>');
